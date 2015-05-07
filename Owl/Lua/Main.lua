47a23aaff5684f7b424ca08814ff3a257fe1fcb4
edgekillerfr <edgekillerfr@live.fr>
2015-05-07 20:56:19 +0200

    Main.lua update

--- Owl/Lua/Main.lua
+++ Owl/Lua/Main.lua
@@ -4,11 +4,18 @@ import ('Microsoft.Xna.Framework.Input')
 import ('MonoGame.Framework')
 import ('Owl')
 
-local pos = Vector2(50,50)
-local sp = Sprite("logo_512.png", pos)
+--[[ Global variables :
+	spriteBatch -> Default SpriteBatch
+	graphics -> Default GraphicsDeviceManager
+	content -> Default ContentManager
+	game -> Default Game
+--]]
+
+local sp = Sprite("logo_32.png", Vector2(50,50))
+local spSpeed = 2
 
 function Init()
-
+	game.Window.Title = "Example Game"
 end
 
 function Load()
@@ -19,8 +26,24 @@ function Unload()
 
 end
 
-function Update(gameTime)
-
+function Update(gameTime, keyboard, mouse)
+		
+	if(keyboard:IsKeyDown(Keys.Z)) then
+		sp:SetPosition(sp:GetPosition().X, sp:GetPosition().Y - spSpeed)
+	end
+	
+	if(keyboard:IsKeyDown(Keys.S)) then
+		sp:SetPosition(sp:GetPosition().X, sp:GetPosition().Y + spSpeed)
+	end
+	
+	if(keyboard:IsKeyDown(Keys.Q)) then
+		sp:SetPosition(sp:GetPosition().X - spSpeed, sp:GetPosition().Y)
+	end
+	
+	if(keyboard:IsKeyDown(Keys.D)) then
+		sp:SetPosition(sp:GetPosition().X + spSpeed, sp:GetPosition().Y)
+	end
+		
 end
 
 function Draw(gameTime)
