4e34c5f527319640145c1bd8570a49f87b67c492
edgekillerfr <edgekillerfr@live.fr>
2015-05-08 22:08:28 +0200

    Big update

--- Owl/Framework/Sprite.cs
+++ Owl/Framework/Sprite.cs
@@ -7,13 +7,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.IO;
 
-namespace Owl
+namespace Owl.Framework
 {
     public class Sprite
     {
         private Texture2D texture;
         private string texturePath;
         private Vector2 position;
+        private GraphicsDeviceManager gdm;
 
         public Sprite(string texturePath, Vector2 position)
         {
@@ -23,6 +24,7 @@ namespace Owl
 
         public void Load(GraphicsDeviceManager graphicsDeviceManager)
         {
+            gdm = graphicsDeviceManager;
             using(FileStream fs = new FileStream(this.texturePath, FileMode.Open))
             {
                 this.texture = Texture2D.FromStream(graphicsDeviceManager.GraphicsDevice, fs);
@@ -32,11 +34,11 @@ namespace Owl
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, position, Color.White);
-
-            
         }
 
         // GET & SET
+
+        //  Position
         public void SetPosition(Vector2 position)
         {
             this.position = position;
@@ -45,11 +47,54 @@ namespace Owl
         {
             this.position = new Vector2(x, y);
         }
+        public void SetPositionX(float x)
+        {
+            this.position.X = x;
+        }
+        public void SetPositionY(float y)
+        {
+            this.position.Y = y;
+        }
+        public void AddPosition(Vector2 position)
+        {
+            this.position += position;
+        }
+        public void AddPosition(float x, float y)
+        {
+            this.position += new Vector2(x,y);
+        }
+        public void AddPositionX(float x)
+        {
+            this.position.X += x;
+        }
+        public void AddPositionY(float y)
+        {
+            this.position.Y += y;
+        }
         public Vector2 GetPosition()
         {
             return this.position;
         }
 
-        
+        // Texture
+        public void SetTexture(string texturePath)
+        {
+            this.texturePath = "Resources/Textures/" + texturePath;
+            this.Load(gdm);
+        }
+        public Texture2D GetTexture()
+        {
+            return this.texture;
+        }
+        public string GetTextureName()
+        {
+            return this.texturePath.Replace("Resources/Textures/", "");
+        }
+        public string GetTexturePath()
+        {
+            return this.texturePath;
+        }
+
+
     }
 }
--- Owl/Lua/Main.lua
+++ Owl/Lua/Main.lua
@@ -2,7 +2,7 @@ import ('Microsoft.Xna.Framework')
 import ('Microsoft.Xna.Framework.Graphics')
 import ('Microsoft.Xna.Framework.Input')
 import ('MonoGame.Framework')
-import ('Owl')
+import ('Owl', 'Owl.Framework')
 
 --[[ Global variables :
 	spriteBatch -> Default SpriteBatch
@@ -11,43 +11,50 @@ import ('Owl')
 	game -> Default Game
 --]]
 
-local sp = Sprite("logo_32.png", Vector2(50,50))
+local owl = Sprite("logo_32.png", Vector2(42,42))
 local spSpeed = 2
 
 function Init()
 	game.Window.Title = "Example Game"
+	game.IsMouseVisible = true
 end
 
 function Load()
-	sp:Load(graphics)
+	owl:Load(graphics)
 end
 
 function Unload()
 
 end
 
+cPressed = false
+
 function Update(gameTime, keyboard, mouse)
-		
-	if(keyboard:IsKeyDown(Keys.Z)) then
-		sp:SetPosition(sp:GetPosition().X, sp:GetPosition().Y - spSpeed)
+
+	if(keyboard:IsKeyDown(Keys.Up)) then owl:AddPositionY(-spSpeed) end
+	if(keyboard:IsKeyDown(Keys.Down)) then	owl:AddPositionY(spSpeed) end
+	if(keyboard:IsKeyDown(Keys.Left)) then	owl:AddPositionX(-spSpeed) end
+	if(keyboard:IsKeyDown(Keys.Right)) then	owl:AddPositionX(spSpeed) end
+	
+	
+	if(keyboard:IsKeyDown(Keys.C)) then 
+		if(cPressed == false) then
+			if(owl:GetTextureName() == "logo_32.png") then
+				owl:SetTexture("logo_512.png")
+			else
+				owl:SetTexture("logo_32.png")
+			end
+		end
+		cPressed = true
+	else
+		cPressed = false
 	end
 	
-	if(keyboard:IsKeyDown(Keys.S)) then
-		sp:SetPosition(sp:GetPosition().X, sp:GetPosition().Y + spSpeed)
-	end
-	
-	if(keyboard:IsKeyDown(Keys.Q)) then
-		sp:SetPosition(sp:GetPosition().X - spSpeed, sp:GetPosition().Y)
-	end
-	
-	if(keyboard:IsKeyDown(Keys.D)) then
-		sp:SetPosition(sp:GetPosition().X + spSpeed, sp:GetPosition().Y)
-	end
-		
 end
 
 function Draw(gameTime)
 	spriteBatch:Begin()
-	sp:Draw(spriteBatch)
+	--owl:Draw(spriteBatch)
+	spriteBatch:Draw(owl:GetTexture(), owl:GetPosition(), Color.White)
 	spriteBatch:End()
 end
